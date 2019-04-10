92b9c3882593ad79949676b8886cc9ad7fc5c904
EKostinou <vagelis789@gmail.com>
2019-04-10 18:39:21 +0300

    Implemented bank, player and space classes

--- 2nd Deliverable/Monopoly/player.cpp
+++ 2nd Deliverable/Monopoly/player.cpp
@@ -5,4 +5,51 @@
 
 using namespace std;
 
-// TODO: Implement here the methods of Player
+// Constructor of Player
+Player::Player(int playerId) {
+    id = playerId;
+    location = 0;
+    money = 1500;
+
+    if(id == 0) {
+        name = "Player 1";
+    }
+    else if(id == 1) {
+        name = "Player 2";
+    }
+    else {
+        cout << "id must be 0 or 1. name set to \"Unknown\"" << endl;
+        name = "Unknown";
+    }
+}
+
+// Getter functions
+int Player::getId() {
+    return id;
+}
+
+int Player::getLocation() {
+    return location;
+}
+
+int Player::getMoney() {
+    return money;
+}
+
+string Player::getName() {
+    return name;
+}
+// End of getters
+
+// Setter function for location
+void Player::setLocation(int spaceNum) {
+    location = spaceNum;
+}
+
+void Player::giveMoneyToPlayer(int amount) {
+    money += amount;
+}
+
+void Player::takeMoneyFromPlayer(int amount) {
+    money -= amount;
+}
--- 2nd Deliverable/Monopoly/space.cpp
+++ 2nd Deliverable/Monopoly/space.cpp
@@ -2,4 +2,73 @@
 
 #include "space.h"
 
-// TODO: Implement here the methods of Space
+// Constructor of Space
+Space::Space() {}
+
+// Setter functions
+void Space::setId(int newId) {
+    id = newId;
+}
+
+void Space::setName(string newName) {
+    name = newName;
+}
+
+void Space::setType(string newType) {
+    type = newType;
+}
+
+void Space::setActionText(string newActionText) {
+    actionText = newActionText;
+}
+
+void Space::setRent(int newRent) {
+    rent = newRent;
+}
+
+void Space::setTax(int newTax) {
+    tax = newTax;
+}
+
+void Space::setBuyingCost(int newBuyCost) {
+    buyCost = newBuyCost;
+}
+
+void Space::setOwner(int newOwner) {
+    owner = newOwner;
+}
+// End of setter functions
+
+// Getter functions
+int Space::getId() {
+    return id;
+}
+
+string Space::getName() {
+    return name;
+}
+
+string Space::getType() {
+    return type;
+}
+
+string Space::getActionText() {
+    return actionText;
+}
+
+int Space::getRent() {
+    return rent;
+}
+
+int Space::getTax() {
+    return tax;
+}
+
+int Space::getBuyingCost() {
+    return buyCost;
+}
+
+int Space::getOwner() {
+    return owner;
+}
+// End of getter functions
