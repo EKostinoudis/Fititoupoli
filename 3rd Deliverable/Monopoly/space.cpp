a6da1f7ddb8f6a85c1ed441a983f6e5a2b9dee6d
EKostinou <vagelis789@gmail.com>
2019-05-05 17:23:55 +0300

    Done Space and Property classes

--- 3rd Deliverable/Monopoly/space.cpp
+++ 3rd Deliverable/Monopoly/space.cpp
@@ -1,4 +1,93 @@
 #include <iostream>
 #include "space.h"
 
-// TODO: Implement here the methods of Space, Property, RailRoad, Utility, and Tax
+/* Space class */
+// Space constructor
+Space::Space(int id, string name, SpaceType type, string actionText) {
+    this->id = id;
+    this->name = name;
+    this->type = type;
+    this->actionText = actionText;
+
+    owner = NULL;
+}
+
+// Setter for owner
+void Space::setOwner(Player* owner) {
+    this->owner = owner;
+}
+
+// Getters
+int Space::getId() {
+    return id;
+}
+
+string Space::getName() {
+    return name;
+}
+
+string Space::getActionText() {
+    return actionText;
+}
+// End of getters
+
+bool Space::typeIs(SpaceType type) {
+    return type == this->type;
+}
+
+bool Space::ownerIs(Player* player) {
+    return player->getName() == owner->getName();
+}
+/* End of Space class */
+
+
+/* Property class */
+// Property constructor
+Property::Property(int id, string name, SpaceType type, string actionText, int buyCost, int upgradeCost, PropertyCategory category)
+  : Space(id, name, type, actionText) {
+    this->buyCost = buyCost;
+    this->upgradeCost = upgradeCost;
+    this->category = category;
+
+    numberOfHouses = 0;
+}
+
+// Rent setter
+void Property::setRent(int numberOfHouses, int inputRent) {
+    rent[numberOfHouses] = inputRent;
+}
+
+// Getters
+int Property::getBuyingCost() {
+    return buyCost;
+}
+
+int Property::getUpgradeCost() {
+    return upgradeCost;
+}
+
+int Property::getNumberOfHouses() {
+    return numberOfHouses;
+}
+
+PropertyCategory Property::getCategory() {
+    return category;
+}
+// End of getters ??
+
+int Property::getRent() {
+    return rent[numberOfHouses];
+}
+
+void Property::addHouse() {
+    if(numberOfHouses < 5) {
+        numberOfHouses++;
+    }
+    else {
+        std::cout << "Can't have more that 5 houses." << std::endl;
+    }
+}
+/* End of Property class */
+
+
+// TODO: Implement here the methods of RailRoad, Utility, and Tax
